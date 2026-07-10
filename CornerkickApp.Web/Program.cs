1a346d37a2f7cbd58b298b1a7c1c664bcba528fb
Jan Suhrmann <s.jan@web.de>
2026-07-10 12:30:13 +0200

    Web: Fix root path

--- CornerkickApp.Web/Program.cs
+++ CornerkickApp.Web/Program.cs
@@ -183,7 +183,8 @@ app.MapGet("/api/as3", async (IAmazonS3Service as3service) => {
 //app.MapGet("/api/member/desk").RequireAuthorization();
 
 // Get cornerkick root directory
-string? sRootPath = builder.Environment.ContentRootPath;
+string? sRootPath = AppDomain.CurrentDomain.BaseDirectory;
+if (string.IsNullOrEmpty(sRootPath)) sRootPath = Environment.GetEnvironmentVariable("ckRootPath");
 if (string.IsNullOrEmpty(sRootPath)) sRootPath = builder.Configuration.GetSection("ckRootPath").Value;
 if (string.IsNullOrEmpty(sRootPath)) sRootPath = ".";
 
@@ -191,13 +192,16 @@ if (string.IsNullOrEmpty(sRootPath)) sRootPath = ".";
 string? sCkInstanceName = Environment.GetEnvironmentVariable("ckInstanceName");
 if (string.IsNullOrEmpty(sCkInstanceName)) sCkInstanceName = builder.Configuration.GetSection("ckInstanceName").Value;
 if (string.IsNullOrEmpty(sCkInstanceName)) sCkInstanceName = "";
-
-#if _DEPLOY_ON_HOST // Use environment variable
-string sHomeDir = Path.Combine(sRootPath, "wwwroot", "_content", "cornerkickapp.components");
-#else
-string sHomeDir = Path.Combine(sRootPath, "..", "CornerkickApp.Components", "wwwroot");
-#endif
 CornerkickApp.Shared.Models.CkAppShared.sCkInstanceName = sCkInstanceName == null ? "" : sCkInstanceName;
+
+// Set ck home directory
+string sHomeDir = builder.Environment.ContentRootPath;
+string? sDeployOnHost = Environment.GetEnvironmentVariable("_DEPLOY_ON_HOST");
+if (!string.IsNullOrEmpty(sDeployOnHost) && sDeployOnHost.Equals("true", StringComparison.OrdinalIgnoreCase)) {
+  sHomeDir = Path.Combine(sHomeDir, "wwwroot", "_content", "cornerkickapp.components");
+} else {
+  sHomeDir = Path.Combine(sHomeDir, "..", "CornerkickApp.Components", "wwwroot");
+}
 
 // Compose App_Data dir in Cornerkick.Components
 string sAppDataDir = Path.Combine(sHomeDir, "Content", "Uploads");
@@ -205,7 +209,7 @@ string sAppDataDir = Path.Combine(sHomeDir, "Content", "Uploads");
 // Start Cornerkick
 CornerkickApp.Controllers.App appCk = new CornerkickApp.Controllers.App(
   builder.Configuration,
-  AppDomain.CurrentDomain.BaseDirectory,
+  sRootPath,
   sAppDataDir
 );
 appCk.start();
